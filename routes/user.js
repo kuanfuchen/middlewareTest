3e716f323f9c440f373666b8f9bfecbe51f88f08
kuanfuchen <creazy_fu@hotmail.com>
2022-05-29 15:36:13 +0800

    usersControllers change

--- routes/user.js
+++ routes/user.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const postsController = require('../controllers/userControllers')
+const usersController = require('../controllers/userControllers')
 
 /* GET users listing. */
 router.get('/', (req, res) =>{
-  postsController.getUser(req,res)
+  usersController.getUser(req,res)
 });
 router.post('/',(req,res,next )=>{
-  postsController.createUsers(req,res, next)
+  usersController.createUsers(req,res, next)
 })
 router.delete('/:id',(req,res, next)=>{
-  postsController.deleteOneUser(req,res, next)
+  usersController.deleteOneUser(req,res, next)
 });
 router.delete('/',(req, res, next)=>{
-  postsController.deleteManyUser(req,res, next)
+  usersController.deleteManyUser(req,res, next)
 })
 router.patch('/:id',(req,res, next)=>{
-  postsController.updataUser(req,res, next)
+  usersController.updataUser(req,res, next)
 })
 module.exports = router;
